db5cc6c12adb8c2b79880755f247f1750bf05dbd
ricardowoj <ricardowoj@hotmail.com>
2020-07-03 18:35:10 -0300

    This is a new commit for what I originally planned to be amended

---
+++ usuarios/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+def cadastro (request):
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha_confirmacao = request.POST['password2']
+        if not nome.strip():
+            return redirect('cadastro')
+        if not email.strip():
+            return redirect('cadastro')
+        if senha != senha_confirmacao:
+            return redirect('cadastro')
+        if User.objects.filter(email=email).exists():
+            return redirect('cadastro')
+        user = User.objects.create_user(
+            username=nome,
+            email=email,
+            password=senha
+        )
+        user.save()
+        return redirect('login')
+    else:
+        return render(request, 'usuarios/cadastro.html')
+
+def login(request):
+    if request.method == 'POST': 
+        email = request.POST['email']
+        senha = request.POST['senha']
+        if email == "" or senha == "":
+            return redirect('login')
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list('username', flat=True).get()
+            user = auth.authenticate(request, username=nome, password=senha)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('dashboard')
+    else:
+        return render(request, 'usuarios/login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('index') 
+
+def dashboard(request):
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/dashboard.html')   
+    else:
+        return redirect('index')
+
+def cria_receita(request):
+    if request.method == 'POST':
+        nome_receita = request.POST['nome_receita']
+        ingredientes = request.POST['ingredientes']
+        modo_preparo = request.POST['modo_preparo']
+        tempo_preparo = request.POST['tempo_preparo']
+        rendimento = request.POST['rendimento']
+        categoria = request.POST['categoria']
+        foto_receita = request.FILES['foto_receita']
+        return redirect('dashboard')
+    else:
+        return render(request, 'usuarios/cria_receita.html')
