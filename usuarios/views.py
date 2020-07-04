7d332899611f89c956ab7604cbc519626c5117ea
ricardowoj <ricardowoj@hotmail.com>
2020-07-04 16:29:35 -0300

    projeto final

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
-from django.contrib import auth
+from django.contrib import auth, messages
+from receitas.models import Receita
 
 def cadastro (request):
     if request.method == 'POST':
@@ -13,8 +14,11 @@ def cadastro (request):
         if not email.strip():
             return redirect('cadastro')
         if senha != senha_confirmacao:
+            messages.error(request, 'As senhas não são iguais')
             return redirect('cadastro')
         if User.objects.filter(email=email).exists():
+            return redirect('cadastro')
+        if User.objects.filter(username=nome).exists():
             return redirect('cadastro')
         user = User.objects.create_user(
             username=nome,
@@ -22,6 +26,7 @@ def cadastro (request):
             password=senha
         )
         user.save()
+        messages.success(request, 'Cadastro realizado com sucesso')
         return redirect('login')
     else:
         return render(request, 'usuarios/cadastro.html')
@@ -47,7 +52,12 @@ def logout(request):
 
 def dashboard(request):
     if request.user.is_authenticated:
-        return render(request, 'usuarios/dashboard.html')   
+        id = request.user.id
+        receitas = Receita.objects.order_by('-date_receita').filter(pessoa=id)
+        dados = {
+            'receitas' : receitas
+        }
+        return render(request, 'usuarios/dashboard.html', dados)   
     else:
         return redirect('index')
 
@@ -60,6 +70,9 @@ def cria_receita(request):
         rendimento = request.POST['rendimento']
         categoria = request.POST['categoria']
         foto_receita = request.FILES['foto_receita']
+        user = get_object_or_404(User, pk=request.user.id)
+        receita = Receita.objects.create(pessoa=user, nome_receita=nome_receita, ingredientes=ingredientes, modo_preparo=modo_preparo, tempo_preparo=tempo_preparo, rendimento=rendimento, categoria=categoria, foto_receita=foto_receita)
+        receita.save()
         return redirect('dashboard')
     else:
         return render(request, 'usuarios/cria_receita.html')
